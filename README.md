7bed0b2f60b7b38f904219b742e3f8604d98c6c8
BrandonSRT <brandon.se2009@gmail.com>
2021-09-23 17:55:03 -0600

    Uploading Checkerboard Exercise

---
+++ README.md
@@ -0,0 +1,11 @@
+<h1>Assignment: Checkerboard Exercise</h1>
+</h2>Objectives:</h2>
+    <ul>
+        <li>Continue to learn how to pass information from the url to the route</li>
+        <li>Practice linking static files to templates</li>
+        <li>Get comfortable passing information from the route to the template</li>
+        <li>Understand how to use for loop properly in the template</li>
+        <li>Recognize the value of creating a html/css first and then adding logic/code</li>
+    </ul>
+
+<p><strong>File upload contains all the exercises requested solved.</strong></p>
---
+++ server.py
@@ -0,0 +1,39 @@
+from flask import Flask
+from flask import render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def principal():
+    columnas = 8
+    filas = 8
+    return render_template('index.html', columnas=int(columnas), filas=int(filas))
+
+
+@app.route('/<nfilas>', methods=['GET'])
+def actualiza(nfilas=8):
+    columnas = 8
+    filas = nfilas
+    return render_template('index.html', columnas=int(columnas), filas=int(filas))
+
+
+@app.route('/<nfilas>/<ncolumnas>', methods=['GET'])
+def actualizaFC(nfilas=8, ncolumnas=8):
+    columnas = ncolumnas
+    filas = nfilas
+    return render_template('index.html', columnas=int(columnas), filas=int(filas))
+
+
+@app.route('/<nfilas>/<ncolumnas>/<color1>/<color2>', methods=['GET'])
+def actualizarFCColor(nfilas=8, ncolumnas=8, color1="red", color2="black"):
+    columnas = ncolumnas
+    filas = nfilas
+    nColor1 = color1
+    nColor2 = color2
+    return render_template('index.html', columnas=int(columnas), filas=int(filas), color1=nColor1, color2=nColor2)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
